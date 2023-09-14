86ccf14f9e45a0b1175d13e711c593ae12ba7482
Lucas <lvksounds@gmail.com>
2023-09-14 16:43:06 -0300

    finalizando modulo 6

---
+++ Vue - The Complete Guide (incl. Router & Composition API)/5 - Vue - Behind the Scenes/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.$refs.userText.value;
+      //console.log(this.$refs.userText.value);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created()");
+  },
+
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mouted");
+  },
+
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+
+  updated() {
+    console.log("updated");
+  },
+});
+
+app.mount("#app");
