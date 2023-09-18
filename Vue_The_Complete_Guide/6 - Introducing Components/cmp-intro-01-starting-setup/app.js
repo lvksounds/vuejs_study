0035f8a24b99ba191da81065749d2d72431a087f
Lucas <lvksounds@gmail.com>
2023-09-18 10:44:47 -0300

    .

--- Vue_The_Complete_Guide/6 - Introducing Components/cmp-intro-01-starting-setup/app.js
+++ Vue_The_Complete_Guide/6 - Introducing Components/cmp-intro-01-starting-setup/app.js
@@ -24,8 +24,8 @@ app.component("friend-contact", {
                 <h2>{{ friend.name }}</h2>
                 <button @click="toggleDetails">Show Details</button>
                 <ul v-if="detailsAreVisible">
-                  <li><strong>Phone:</strong> {{friend.phone}}</li>
-                  <li><strong>Email:</strong> {{friend.email}}</li>
+                  <li><strong>Phone:</strong>{{friend.phone}}</li>
+                  <li><strong>Email:</strong>{{friend.email}}</li>
                 </ul>
               </li>`,
   data() {
