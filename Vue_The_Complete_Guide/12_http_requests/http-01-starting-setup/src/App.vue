3030112db6cd950dcb643f30010e5ffd03960bfe
Lucas <lvksounds@gmail.com>
2024-03-01 15:36:45 -0300

    .

--- Vue_The_Complete_Guide/12_http_requests/http-01-starting-setup/src/App.vue
+++ Vue_The_Complete_Guide/12_http_requests/http-01-starting-setup/src/App.vue
@@ -17,17 +17,17 @@ export default {
       savedSurveyResults: [],
     };
   },
-  methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
-  },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
 };
 </script>
 
